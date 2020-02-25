a418379ee476dc63c361358530916b09e10dd5c9
Daria Glagol <daria.glagol@gmail.com>
2020-02-25 13:28:22 +0700

    add mocks

---
+++ src/mocks/films.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    title: `Fantastic Beasts: The Crimes of Grindelwald`,
+    posterUrl: `img/fantastic-beasts-the-crimes-of-grindelwald.jpg`
+  },
+  {
+    title: `Bohemian Rhapsody`,
+    posterUrl: `img/bohemian-rhapsody.jpg`
+  },
+  {
+    title: `Macbeth`,
+    posterUrl: `img/macbeth.jpg`
+  },
+  {
+    title: `Aviator`,
+    posterUrl: `img/aviator.jpg`
+  },
+  {
+    title: `We need to talk about Kevin`,
+    posterUrl: `img/we-need-to-talk-about-kevin.jpg`
+  },
+  {
+    title: `What We Do in the Shadows`,
+    posterUrl: `img/what-we-do-in-the-shadows.jpg`
+  },
+  {
+    title: `Revenant`,
+    posterUrl: `img/revenant.jpg`
+  },
+  {
+    title: `Johnny English`,
+    posterUrl: `img/johnny-english.jpg`
+  },
+];
