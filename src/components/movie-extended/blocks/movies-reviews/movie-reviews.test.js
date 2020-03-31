d8bb798f5ee6b8ccfaecc04aa8291b8b5b2a75db
Daria Glagol <daria.glagol@gmail.com>
2020-03-31 23:25:43 +0700

    make mocks simple as i can

--- src/components/movie-extended/blocks/movies-reviews/movie-reviews.test.js
+++ src/components/movie-extended/blocks/movies-reviews/movie-reviews.test.js
@@ -2,16 +2,16 @@ import React from "react";
 import renderer from "react-test-renderer";
 import MovieReviews from "./movies-reviews";
 
-const MovieReviewsMock = [
+const movieReviewsMock = [
   {
-    text: `Discerning travellers and Wes Anderson fans will luxuriate in the glorious Mittel-European kitsch of one of the director&apos;s funniest and most exquisitely designed movies in years.`,
-    author: `Kate Muir`,
-    date: `December 24, 2016`,
+    text: `text`,
+    author: `author`,
+    date: `date`,
     rating: 8.9
   }, {
-    text: `Anderson&apos;s films are too precious for some, but for those of us willing to lose ourselves in them, they&apos;re a delight. &quot;The Grand Budapest Hotel&quot; is no different, except that he has added a hint of gravitas to the mix, improving the recipe.`,
-    author: `Bill Goodykoontz`,
-    date: `November 18, 2015`,
+    text: `text`,
+    author: `author`,
+    date: `date`,
     rating: 8.0
   }
 ];
@@ -20,7 +20,7 @@ it(`Render movie reviews`, () => {
   const movieReviewsComponent = renderer
     .create(
         <MovieReviews
-          reviews={MovieReviewsMock}
+          reviews={movieReviewsMock}
         />
     ).toJSON();
 
